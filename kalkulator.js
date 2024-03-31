4df3fcdc09db8e2f66b3f711c983713b93cc6de7
Szymon Szulkowski <szymon.p.szulkowski@gmail.com>
2024-03-31 09:04:09 +0200

    changed the code to use arrow functions

--- kalkulator.js
+++ kalkulator.js
@@ -1,43 +1,69 @@
-
-let selectElement = document.querySelector(".js-select");
-let kursElement = document.querySelector(".js-kurs");
-let wartoscElement = document.querySelector(".js-wartosc")
-let wynikElement = document.querySelector(".js-wynik");
-
-const kursEuro = 4.6421;
-const kursUsd = 4.3447;
-const kursFunt = 5.3807;
-const kursPeso = 0.2536;
-const kursSzyling = 0.0017;
-
-const init = () => {
+{
     const formElement = document.querySelector(".js-form");
-}
+    const selectElement = document.querySelector(".js-select");
+    const wartoscElement = document.querySelector(".js-wartosc");
+    const kursElement = document.querySelector(".js-kurs");
+    const wynikElement = document.querySelector(".js-wynik");
 
-formElement.addEventListener("input", () => {
-    if (selectElement.value === "euro") {
+    const init = () => {
+        wynikElement.innerText = "Wybierz walutę aby rozpocząć";
+    };
+    formElement.addEventListener("input", () => {
+        if (selectElement.value === "euro") {
+            euro(wartoscElement.value);
+        }
+        else if (selectElement.value === "usd") {
+            usd(wartoscElement.value);
+        }
+        else if (selectElement.value === "funt") {
+            funt(wartoscElement.value);
+        }
+        else if (selectElement.value === "peso") {
+            peso(wartoscElement.value);
+        }
+        else if (selectElement.value === "szyling") {
+            szyling(wartoscElement.value);
+        }
+    });
+
+    const euro = () => {
+        const kursEuro = 4.6421;
+
         kursElement.innerText = `Aktualny kurs: ${kursEuro}`;
         wynikElement.innerText = `Twoja wartość w przybliżeniu wynosi: ${(wartoscElement.value * kursEuro).toFixed(2)} złotych`;
         console.log("Wybrane Euro");
-    }
-    else if (selectElement.value === "usd") {
+
+    };
+    const usd = () => {
+        const kursUsd = 4.3447;
         kursElement.innerText = `Aktualny kurs: ${kursUsd}`;
         wynikElement.innerText = `Twoja wartość w przybliżeniu wynosi: ${(wartoscElement.value * kursUsd).toFixed(2)} złotych`;
         console.log("Wybrane Usd");
-    }
-    else if (selectElement.value === "funt") {
+
+    };
+    const funt = () => {
+        const kursFunt = 5.3807;
         kursElement.innerText = `Aktualny kurs: ${kursFunt}`;
         wynikElement.innerText = `Twoja wartość w przybliżeniu wynosi: ${(wartoscElement.value * kursFunt).toFixed(2)} złotych`;
         console.log("Wybrane Funty");
-    }
-    else if (selectElement.value === "peso") {
+
+    };
+    const peso = () => {
+        const kursPeso = 0.2536;
         kursElement.innerText = `Aktualny kurs: ${kursPeso}`;
         wynikElement.innerText = `Twoja wartość w przybliżeniu wynosi: ${(wartoscElement.value * kursPeso).toFixed(2)} złotych`;
         console.log("Wybrane Peso");
-    }
-    else if (selectElement.value === "szyling") {
+
+    };
+
+    const szyling = () => {
+        const kursSzyling = 0.0017;
         kursElement.innerText = `Aktualny kurs: ${kursSzyling}`;
         wynikElement.innerText = `Twoja wartość w przybliżeniu wynosi: ${(wartoscElement.value * kursSzyling).toFixed(2)} złotych`;
         console.log("Wybrane Szyling");
-    }
-});
+
+    };
+
+    init();
+
+};
